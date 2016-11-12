099af5948accce102b88856a413e6464effa1088
Hugo <alhugone@gmail.com>
2016-11-12 03:51:23 +0100

    Added ActivityTracker.Events project with events

---
+++ src/ActivityTracker.Events/Events.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ActivityTracker.Events
+{
+    public interface IEvent
+    {
+        DateTimeOffset Timestamp { get; }
+    }
+
+    public abstract class Event : IEvent
+    {
+        public DateTimeOffset Timestamp { get; } = DateTimeOffset.Now;
+    }
+    public class ServiceStartedEvent : Event { }
+    public class ServiceStoppedEvent : Event { }
+}
