5e3b52e265f52c7b4646fb4fb3c37248562156df
Hugo <alhugone@gmail.com>
2016-12-05 17:33:52 +0100

    FileEventStore add

---
+++ src/FileEventStore/FileEventStore.cs
@@ -0,0 +1,29 @@
+﻿using ActivityTracker.Events;
+using ActivityTracker.EventStore;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace FileEventStore
+{
+    public class FileEventStore : IEventStore
+    {
+        private readonly string _fileFullPath;
+
+        public FileEventStore(string fileFullPath)
+        {
+            Validate(fileFullPath);
+            _fileFullPath = fileFullPath;
+        }
+
+        private void Validate(string rootPath)
+        {
+            Directory.CreateDirectory(Path.GetDirectoryName(rootPath));
+            File.Create(rootPath, 512, FileOptions.SequentialScan | FileOptions.WriteThrough);
+        }
+
+        public void Store(IActivityEvent activityEvent)
+        {
+            File.AppendAllText(_fileFullPath, JsonConvert.SerializeObject(activityEvent));
+        }
+    }
+}
