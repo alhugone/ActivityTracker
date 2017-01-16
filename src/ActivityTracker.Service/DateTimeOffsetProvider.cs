daec4c79a7e76bb3d6c3685e6d045e6cbfb2a907
Hugo <alhugone@gmail.com>
2017-01-16 14:43:58 +0100

    Added missing DateTimeOffsetProvider.cs files

---
+++ src/ActivityTracker.Service/DateTimeOffsetProvider.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace ActivityTracker.Service
+{
+    public class DateTimeOffsetProvider : IDateTimeOffsetFactory
+    {
+        public DateTimeOffset Now => DateTimeOffset.Now;
+    }
+}
---
+++ src/ActivityTracker.Service/IDateTimeOffsetFactory.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace ActivityTracker.Service
+{
+    public interface IDateTimeOffsetFactory
+    {
+        DateTimeOffset Now { get; }
+    }
+}
